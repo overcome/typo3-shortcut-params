39bd1dc13473854945239db933d7b963d57f28c5
luye <ye.lu2001@outlook.com>
2017-04-15 23:17:58 +0800

    Change hard code and typo3 version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF['shortcut_params'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Shortcut Params',
     'description' => 'Add parameters to page shortcuts',
     'category' => 'frontend',
@@ -11,7 +11,7 @@ $EM_CONF['shortcut_params'] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.2-7.99.99',
+            'typo3' => '7.6.2-8.99.99',
         ],
     ],
 ];
